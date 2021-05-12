bbd282704cba6d0bb38d8c87cf02df710ae03be1
Vanessa Su <vnessa.su@gmail.com>
2021-05-12 13:57:21 -0400

    :white_check_mark: Add constructor test
    Co-Authored-By: Marney Mallory <marney.mallory@gmail.com>
    co-authored-by: Brandon Magofna <Magofna68@gmail.com>

--- Tamagotchi.Tests/ModelTests/PetTests.cs
+++ Tamagotchi.Tests/ModelTests/PetTests.cs
@@ -1,1 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tamagotchi.Models;
+
+namespace Tamagotchi.Tests
+{
+  [TestClass]
+  public class PetTests
+  {
+    [TestMethod]
+    public void PetConstructor_CreatesInstanceOfPet_Pet()
+    {
+      string inputName = "Baldwin";
+      Pet petObject = new Pet(inputName);
+      Assert.AreEqual(typeof(Pet), petObject.GetType());
+      Assert.AreEqual(inputName, petObject.Name);
+    }
+  }
+}
+
+//     [TestMethod]
+//     public void ItemConstructor_CreatesInstanceOfItem_Item()
+//     {
+//       Item newItem = new Item("test");
+//       Assert.AreEqual(typeof(Item), newItem.GetType());
+//     }
