71ef6b6e5804699176d3decb5e01b156c2a6de06
Vanessa Su <vnessa.su@gmail.com>
2021-05-12 14:47:16 -0400

    :white_check_mark: Add test for FeedPet increases Food by 10
    Co-Authored-By: Marney Mallory <marney.mallory@gmail.com>
    Co-Authored-By: Brandon Magofna <Magofna68@gmail.com>

--- Tamagotchi.Tests/ModelTests/PetTests.cs
+++ Tamagotchi.Tests/ModelTests/PetTests.cs
@@ -6,20 +6,39 @@ namespace Tamagotchi.Tests
   [TestClass]
   public class PetTests
   {
+    private static string _inputName = "Baldwin";
+    private Pet _petObject = new Pet(_inputName);
+
     [TestMethod]
-    public void PetConstructor_CreatesInstanceOfPet_Pet()
+    public void PetConstructor_BaldwinPetObject_PetObject()
     {
-      string inputName = "Baldwin";
-      Pet petObject = new Pet(inputName);
-      Assert.AreEqual(typeof(Pet), petObject.GetType());
-      Assert.AreEqual(inputName, petObject.Name);
+      Assert.AreEqual(typeof(Pet), _petObject.GetType());
+      Assert.AreEqual(_inputName, _petObject.Name);
+    }
+
+    [TestMethod]
+    public void FeedPet_FeedInstanceOfPet_IncreaseFoodBy10()
+    {
+    //Arrange
+    _petObject.Food = 90;
+    //Act
+    _petObject.FeedPet();
+    //Assert
+    Assert.AreEqual(100, _petObject.Food);
     }
   }
 }
 
-//     [TestMethod]
-//     public void ItemConstructor_CreatesInstanceOfItem_Item()
-//     {
-//       Item newItem = new Item("test");
-//       Assert.AreEqual(typeof(Item), newItem.GetType());
-//     }
+    // [TestMethod]
+    // public void GetTask_ReturnsTask_String()
+    // {
+    //   //Arrange
+    //   string description = "Walk the dog.";
+
+    //   //Act
+    //   Item newItem = new Item(description);
+    //   string result = newItem.Task;
+
+    //   //Assert
+    //   Assert.AreEqual(description, result);
+    // }
