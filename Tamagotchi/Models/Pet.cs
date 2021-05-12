732634f68f68126dcd41e3255f03fa8b7cfc59f6
Vanessa Su <vnessa.su@gmail.com>
2021-05-12 14:47:48 -0400

    :hammer: Pass FeedPet increases Food by 10 test
    Co-Authored-By: Marney Mallory <marney.mallory@gmail.com>
    Co-Authored-By: Brandon Magofna <Magofna68@gmail.com>

--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -3,10 +3,17 @@ namespace Tamagotchi.Models
   public class Pet
   {
     public string Name {get; set;}
+    public int Food {get; set;}
 
     public Pet(string name)
     {
       Name = name;
+      Food = 100;
+    }
+
+    public int FeedPet()
+    {
+      return Food += 10;
     }
   }
 }
